fe0005171e9659226a7bf34163348f3944e1c122
dhaag23 <dirk@toktumi.com>
2011-02-25 16:41:22 -0800

    Allow configuration of remote API URL as well as server IP address.

--- gen/com/toktumi/line2/config/R.java
+++ gen/com/toktumi/line2/config/R.java
@@ -14,10 +14,11 @@ public final class R {
         public static final int app_icon=0x7f020000;
     }
     public static final class id {
-        public static final int ip_address=0x7f050000;
-        public static final int save=0x7f050002;
-        public static final int show_vqe_recording=0x7f050003;
-        public static final int type=0x7f050001;
+        public static final int ip_address=0x7f050001;
+        public static final int remote_api_url=0x7f050002;
+        public static final int save=0x7f050003;
+        public static final int show_debug_settings=0x7f050004;
+        public static final int type=0x7f050000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/com/toktumi/line2/config/Main.java
+++ src/com/toktumi/line2/config/Main.java
@@ -1,5 +1,7 @@
 package com.toktumi.line2.config;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -25,22 +27,25 @@ import android.widget.Toast;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 
-public class Main extends Activity implements TextWatcher {
+public class Main extends Activity {
 	public static Context MainActivity;
 	
 	// Internal IP for softphone configuration
 	private final static String INTERNAL_CONFIG_IP_EXTRA = "com.toktumi.intent.extra.INTERNAL_CONFIG_IP";
+	// Internal Remote API URL for softphone configuration
+	public final static String INTERNAL_CONFIG_REMOTE_API_URL_EXTRA = "com.toktumi.intent.extra.INTERNAL_CONFIG_REMOTE_API_URL";
 	// IP type for softphone configuration
 	private final static String INTERNAL_CONFIG_TYPE_EXTRA = "com.toktumi.intent.extra.INTERNAL_CONFIG_TYPE";
-	// Show VQE recording boolean for softphone configuration
-	private final static String INTERNAL_CONFIG_SHOW_VQE_EXTRA = "com.toktumi.intent.extra.INTERNAL_CONFIG_SHOW_VQE";
+	// Show debug settings (i.e. VQE recording) boolean for softphone configuration
+	public final static String INTERNAL_CONFIG_SHOW_DEBUG_EXTRA = "com.toktumi.intent.extra.INTERNAL_CONFIG_SHOW_DEBUG";
 
 	private static final String[] mTypes = { "Prod", "Dev", "Test", "Stage", "Other" };
 
 	private Spinner typeSpinner;
     private TextView ipAddress;
+    private TextView remoteApiUrl;
     private Button saveButton;
-    private CheckBox showVQERecordingCheck;
+    private CheckBox showSecretDebugSettingsCheck;
 
     public Main() {
     	MainActivity = this;
@@ -60,17 +65,18 @@ public class Main extends Activity implements TextWatcher {
         typeSpinner.setOnItemSelectedListener(onTypeSelected);
  
         ipAddress = (TextView)findViewById(R.id.ip_address);
-        ipAddress.addTextChangedListener(this);
+        ipAddress.addTextChangedListener(ipAddressTextWatcher);
        
+        remoteApiUrl = (TextView)findViewById(R.id.remote_api_url);
+        remoteApiUrl.addTextChangedListener(remoteApiUrlTextWatcher);
+
         saveButton = (Button)findViewById(R.id.save);
 		saveButton.setOnClickListener(onSaveButtonClick);
 		
-		showVQERecordingCheck = (CheckBox)findViewById(R.id.show_vqe_recording);
-		showVQERecordingCheck.setOnCheckedChangeListener(onShowVQERecordingCheckChanged);
+		showSecretDebugSettingsCheck = (CheckBox)findViewById(R.id.show_debug_settings);
+		showSecretDebugSettingsCheck.setOnCheckedChangeListener(onShowSecretDebugSettingsCheckChanged);
 		
 		Intent intent = new Intent("com.toktumi.intent.action.ACTION_INTERNAL_GET_CONFIG");
-		intent.putExtra(INTERNAL_CONFIG_IP_EXTRA, ipAddress.getText().toString());
-		intent.putExtra(INTERNAL_CONFIG_TYPE_EXTRA, typeSpinner.getSelectedItem().toString());
 		sendOrderedBroadcast(intent, null, getInfoResultReceiver, null, 0, null, null);
     }
 
@@ -82,21 +88,27 @@ public class Main extends Activity implements TextWatcher {
 			if (bundle != null) {
 				String softphoneServerIP = bundle.getString(INTERNAL_CONFIG_IP_EXTRA);
 				String softphoneServerType = bundle.getString(INTERNAL_CONFIG_TYPE_EXTRA);
-				boolean showVQERecording = bundle.getBoolean(INTERNAL_CONFIG_SHOW_VQE_EXTRA, false);
-				Log.d("Line2-Config", "Get config returns: server IP address " + softphoneServerIP + " for " + softphoneServerType + "(showVQERecording=" + showVQERecording + ")");
+				String remoteApiUrlSetting = bundle.getString(INTERNAL_CONFIG_REMOTE_API_URL_EXTRA);
+				boolean showSecretDebugSettings = bundle.getBoolean(INTERNAL_CONFIG_SHOW_DEBUG_EXTRA, false);
+				
+				Log.d("Line2-Config", "Get config returns: server IP address " + softphoneServerIP +
+						" for " + softphoneServerType +
+						" and " + remoteApiUrl +
+						" (showSecretDebugSettings=" + showSecretDebugSettings + ")");
 				
 				typeSpinner.setSelection(typeToPosition(softphoneServerType));
 				ipAddress.setText(softphoneServerIP);
-				showVQERecordingCheck.setChecked(showVQERecording);
+				remoteApiUrl.setText(remoteApiUrlSetting);
+				showSecretDebugSettingsCheck.setChecked(showSecretDebugSettings);
 			}
 		}
 	};
     
-	private OnCheckedChangeListener onShowVQERecordingCheckChanged = new OnCheckedChangeListener() {
+	private OnCheckedChangeListener onShowSecretDebugSettingsCheckChanged = new OnCheckedChangeListener() {
 		@Override
 		public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 			Intent intent = new Intent("com.toktumi.intent.action.ACTION_INTERNAL_CONFIG_VQE");
-			intent.putExtra(INTERNAL_CONFIG_SHOW_VQE_EXTRA, isChecked);
+			intent.putExtra(INTERNAL_CONFIG_SHOW_DEBUG_EXTRA, isChecked);
 			sendBroadcast(intent);
 		}
 	};
@@ -107,8 +119,10 @@ public class Main extends Activity implements TextWatcher {
 		public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 			if (position == 0) {
 				ipAddress.setText("67.159.187.100,67.159.187.104");
+				remoteApiUrl.setText("https://www.toktumi.com");
 			}
 			ipAddress.setText(loadIP(position));
+			remoteApiUrl.setText(loadUrl(position));
 		}
 
 		@Override public void onNothingSelected(AdapterView<?> parent) { }
@@ -122,6 +136,7 @@ public class Main extends Activity implements TextWatcher {
 				Intent intent = new Intent("com.toktumi.intent.action.ACTION_INTERNAL_CONFIG");
 				intent.putExtra(INTERNAL_CONFIG_IP_EXTRA, ipAddress.getText().toString());
 				intent.putExtra(INTERNAL_CONFIG_TYPE_EXTRA, typeSpinner.getSelectedItem().toString());
+				intent.putExtra(INTERNAL_CONFIG_REMOTE_API_URL_EXTRA, remoteApiUrl.getText().toString());
 				sendBroadcast(intent);
 	
 				Toast.makeText(Main.this, "Setting update sent. if this worked, Line2 will restart in a few seconds", Toast.LENGTH_LONG).show();
@@ -129,11 +144,35 @@ public class Main extends Activity implements TextWatcher {
 		}
 	};
 
-	@Override
-	public void afterTextChanged(Editable s) {
-		boolean isEmpty = TextUtils.isEmpty(s.toString());
-		saveButton.setEnabled(!isEmpty);
-		saveIP();
+	private TextWatcher ipAddressTextWatcher = new TextWatcher() {
+		
+		@Override public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+		@Override public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+		@Override
+		public void afterTextChanged(Editable s) {
+			updateButtonEnabling();
+			saveIP(typeSpinner.getSelectedItem().toString());
+		}
+	};
+
+	private TextWatcher remoteApiUrlTextWatcher = new TextWatcher() {
+		
+		@Override public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+		@Override public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+		@Override
+		public void afterTextChanged(Editable s) {
+			updateButtonEnabling();
+			saveUrl(typeSpinner.getSelectedItem().toString());
+		}
+	};
+	
+	private void updateButtonEnabling() {
+		boolean isIPAddressEmpty = TextUtils.isEmpty(ipAddress.toString());
+		boolean isRemoteApiUrlEmpty = TextUtils.isEmpty(remoteApiUrl.toString());
+
+		saveButton.setEnabled(!isIPAddressEmpty && !isRemoteApiUrlEmpty);
 	}
 
 	private static String IP_PATTERN = "\\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b";
@@ -147,20 +186,35 @@ public class Main extends Activity implements TextWatcher {
 				return false;
             }
 		}
+
+		try {
+			new URI(remoteApiUrl.getText().toString());
+		} catch (URISyntaxException e) {
+			Toast.makeText(Main.this, "Invalid Remote API Url", Toast.LENGTH_LONG).show();
+			return false;
+		}
+		
 		return true;
 	}
 
-	@Override public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
-	@Override public void onTextChanged(CharSequence s, int start, int before, int count) { }
-
-	private void saveIP() {
+	private void saveIP(String type) {
 		AppSettings appSettings = new AppSettings();
-		appSettings.setStringPref(typeSpinner.getSelectedItem().toString(), ipAddress.getText().toString());
+		appSettings.setStringPref(type, ipAddress.getText().toString());
+	}
+
+	private void saveUrl(String type) {
+		AppSettings appSettings = new AppSettings();
+		appSettings.setStringPref(getRemoteApiPref(type), remoteApiUrl.getText().toString());
 	}
 
 	private String loadIP(int position) {
 		AppSettings appSettings = new AppSettings();
 		return appSettings.getStringPref(mTypes[position], "");
+	}
+
+	private String loadUrl(int position) {
+		AppSettings appSettings = new AppSettings();
+		return appSettings.getStringPref(getRemoteApiPref(mTypes[position]), "");
 	}
 
 	private int typeToPosition(String softphoneServerType) {
@@ -169,5 +223,9 @@ public class Main extends Activity implements TextWatcher {
 				return i;
 		}
 		return 0;
+	}
+	
+	String getRemoteApiPref(String type) {
+		return type + "Url";
 	}
 }
